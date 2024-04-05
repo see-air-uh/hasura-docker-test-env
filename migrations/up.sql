09d4096cff8160e4b93edad853f557c072b6c40b
Luca Sciarra <sciarra.l.m@gmail.com>
2024-04-05 16:33:22 -0400

    change up.sql

--- migrations/up.sql
+++ migrations/up.sql
@@ -2,3 +2,8 @@ CREATE TABLE public.test1 (
     id uuid NOT NULL
     created_at timestamp without time zone DEFAULT now() NOT NULL
 );
+
+CREATE TABLE public.test2 (
+    id uuid NOT NULL
+    created_at timestamp without time zone DEFAULT now() NOT NULL
+);
